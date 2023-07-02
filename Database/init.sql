edc91e4dc0730c5be507ca2392e4e9ad22530b0a
Erik Silva Maia <erikm384@gmail.com>
2023-07-02 02:02:42 -0300

    feat: create script to insert data

---
+++ Database/init.sql
@@ -0,0 +1,7 @@
+desc Material;
+INSERT INTO Material(MaterialType,Link,Title,Image) VALUES (1,'','','');
+SELECT * FROM Material;
+SHOW tables;
+desc Quiz;
+INSERT INTO Quiz(Correct,Questions,FirstOption,SecondOption,ThirdOption) VALUES (2,'','','','');
+SELECT * FROM Quiz;
