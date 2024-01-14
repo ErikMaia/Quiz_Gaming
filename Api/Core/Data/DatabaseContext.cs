0e336b82d66a6b09fc1ae5f9c353783a48b96ec0
Erik Silva Maia <erikm384@gmail.com>
2024-01-14 20:22:39 -0300

    change: order in docker-compose

---
+++ Api/Core/Data/DatabaseContext.cs
@@ -0,0 +1,37 @@
+using api.core.Models;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+public class DatabaseContext : DbContext
+{
+    public DbSet<MaterialModel>? Material { get; set; }
+    public DbSet<QuizzModel>? Quiz { get; set; }
+    public DbSet<StudentModel>? Student { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var serverVersion = new MySqlServerVersion(new Version(8, 0, 31)); // Especifique a versão correta do servidor MySQL aqui
+
+        optionsBuilder.UseMySql("Server=database;Port=3306;Database=tarefas;User=root;Password=tarefas;",
+            serverVersion,
+            options => options.EnableRetryOnFailure()
+        );
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<MaterialModel>().HasData(
+            new MaterialModel
+            {
+                MaterialType = 1,
+                Link = '',
+                Title = '',
+                Image = ''
+            },
+            new MaterialModel
+            {
+                MaterialType = 1,
+                Link = '',
+                Title = '',
+                Image = ''
+            },
+        );
+    }
+}
