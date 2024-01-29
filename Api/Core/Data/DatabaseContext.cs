5b7bb8774a9f66c50fad041423cdd64082330825
Erik Silva Maia <erikm384@gmail.com>
2024-01-29 19:41:25 -0300

    remove useless scripsts

--- Api/Core/Data/DatabaseContext.cs
+++ Api/Core/Data/DatabaseContext.cs
@@ -14,55 +14,5 @@ public class DatabaseContext : DbContext
             serverVersion,
             options => options.EnableRetryOnFailure()
         );
-
     }
-    // protected override void OnModelCreating(ModelBuilder modelBuilder)
-    // {
-    //     modelBuilder.Entity<MaterialModel>().HasData(
-    //         new
-    //         {
-
-    //             MaterialType = 1,
-    //             Link = 'assets/1000-frases-mais-usadas-no-ingles.pdf',
-    //             Title = '1000 Frases Mais Usadas No Ingles',
-    //             Image = 'https://www.baixelivros.com.br/media/2022/02/frases-ingles.jpg'
-    //         }
-    //         ,
-    //         new
-    //         {
-    //             MaterialType = 1,
-    //             Link = 'assets/ingles-aplicado-a-eventos.pdf',
-    //             Title = 'Ingles Aplicado A Eventos',
-    //             Image = 'https://www.baixelivros.com.br/media/2022/02/ingles-aplicado-eventos.jpg'
-    //         }
-    //     );
-    //     modelBuilder.Entity<QuizzModel>().HasData(
-    //         new QuizzModel
-    //         {
-    //             QuizzTitle = 'To Be',
-    //             Correct = 1,
-    //             Questions = 'Qual é a forma correta do verbo "to be" na terceira pessoa do singular no presente simples?',
-    //             FirstOption = 'is',
-    //             SecondOption = 'are',
-    //             ThirdOption = 'am',
-    //         },
-    //         new QuizzModel
-    //         {
-    //             QuizzTitle = 'Pronome',
-    //             Correct = 3,
-    //             Questions = 'Qual das seguintes palavras é um pronome pessoal no caso objetivo?',
-    //             FirstOption = 'she',
-    //             SecondOption = 'we',
-    //             ThirdOption = 'me',
-    //         },
-    //         new QuizzModel
-    //         {
-    //             QuizzTitle = 'Comparativo',
-    //             Correct = 2,
-    //             Questions = 'Qual é a forma correta do comparativo de superioridade do adjetivo "good" (bom)?',
-    //             FirstOption = 'goodest',
-    //             SecondOption = 'better',
-    //             ThirdOption = 'best',
-    //         });
-    // }
 }
